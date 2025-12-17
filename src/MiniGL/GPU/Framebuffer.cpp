c3fa0f0383c8df6ff7dbc03f9e6b2b0207bd6700
Arthur <arthur.pradier81@gmail.com>
2025-12-17 15:50:23 +0100

    feature(Framebuffer): File creation, no code
    Just to not have to do it again now that they're here

---
+++ src/MiniGL/GPU/Framebuffer.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Bellaedris on 16/12/2025.
+//
+
+#include "Framebuffer.h"
+
+namespace mgl::gpu
+{
+
+}
---
+++ src/MiniGL/GPU/Framebuffer.h
@@ -0,0 +1,18 @@
+//
+// Created by Bellaedris on 16/12/2025.
+//
+
+#pragma once
+
+#include <cstdint>
+
+namespace mgl::gpu
+{
+class Framebuffer
+{
+private:
+    uint32_t m_handle;
+public:
+
+};
+}
